4a5575f2f1c31dfd084b4b2cabc6701c43e1edd7
parse-nip <152457438+parse-nip@users.noreply.github.com>
2025-10-23 16:20:52 +0800

    Updated package.json

--- bindings.sh
+++ bindings.sh
@@ -2,14 +2,21 @@
 
 bindings=""
 
-while IFS= read -r line || [ -n "$line" ]; do
-  if [[ ! "$line" =~ ^# ]] && [[ -n "$line" ]]; then
-    name=$(echo "$line" | cut -d '=' -f 1)
-    value=$(echo "$line" | cut -d '=' -f 2-)
-    value=$(echo $value | sed 's/^"\(.*\)"$/\1/')
-    bindings+="--binding ${name}=${value} "
-  fi
-done < .env.local
+ENV_FILE=".env.local"
+if [ ! -f "$ENV_FILE" ]; then
+  ENV_FILE=".env"
+fi
+
+if [ -f "$ENV_FILE" ]; then
+  while IFS= read -r line || [ -n "$line" ]; do
+    if [[ ! "$line" =~ ^# ]] && [[ -n "$line" ]]; then
+      name=$(echo "$line" | cut -d '=' -f 1)
+      value=$(echo "$line" | cut -d '=' -f 2-)
+      value=$(echo $value | sed 's/^"\(.*\)"$/\1/')
+      bindings+="--binding ${name}=${value} "
+    fi
+  done < "$ENV_FILE"
+fi
 
 bindings=$(echo $bindings | sed 's/[[:space:]]*$//')
 
